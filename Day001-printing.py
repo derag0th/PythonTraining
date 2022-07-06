e80fda88eb904a8e8757fc6232c82952c93fadc3
derag0th <azathanai@icloud.com>
2022-07-06 08:53:22 +0300

    Day001 midday

---
+++ Day001-printing.py
@@ -0,0 +1,8 @@
+# Print with new lines with \n
+## print('Hello world!\nHello world!\nHello world!')
+# Use of user input in print function
+## print('Hello ' + input('Hey, what is your name? ') + '!')
+# Printing amount of characters in a string
+a = input('What is your name? ')
+print('Your name is ' + str(len(a)) + ' symbols long.')
+print('Hello ' + a + '.' + '\nHow are you today?')
